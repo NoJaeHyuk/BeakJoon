ab5ec673bae12ceaabd19272eb209b880be871c5
NoJaeHyuk <32382839+NoJaeHyuk@users.noreply.github.com>
2023-07-18 13:07:28 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        StringBuilder s = new StringBuilder();
+
+        s.append("\\    /\\ \n");
+        s.append(" )  ( ')\n");
+        s.append("(  /  )\n");
+        s.append(" \\(__)|\n");
+
+        System.out.println(s);
+        /*
+            \    /\
+             )  ( ')
+            (  /  )
+             \(__)|
+
+         */
+
+    }
+}
