8137e1797f0c6230c2eb75161d22240f6b78fa08
NoJaeHyuk <32382839+NoJaeHyuk@users.noreply.github.com>
2023-07-18 10:36:45 +0900

    [Silver IV] Title: 문서 검색, Time: 232 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,30 @@
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+
+        String b = sc.nextLine();
+
+        int cnt = 0;
+
+        if(a.contains(b)){
+            a = a.replace(b,"X");
+            for(int i = 0; i < a.length(); i++){
+                if(a.charAt(i) == 'X'){
+                    cnt++;
+                }
+            }
+        }else{
+            cnt = 0;
+        }
+
+        System.out.println(cnt);
+
+    }
+}
