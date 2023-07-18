19a4911e573ff97838492544c29432cb3f6622e8
NoJaeHyuk <32382839+NoJaeHyuk@users.noreply.github.com>
2023-07-18 12:17:13 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 220 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int in = scanner.nextInt();
+
+        int bAge = 2541 - 1998;
+        
+        System.out.println(in-bAge);
+    }
+}
