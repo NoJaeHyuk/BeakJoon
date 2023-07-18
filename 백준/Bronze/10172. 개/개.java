c3b67446af170d83f8016899309166483127e495
NoJaeHyuk <32382839+NoJaeHyuk@users.noreply.github.com>
2023-07-18 13:59:20 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder();
+
+        s.append("|\\_/| \n");
+        s.append("|q p|   /} \n");
+        s.append("( 0 )\"\"\"\\ \n");
+        s.append("|\"^\"`    | \n");
+        s.append("||_/=\\\\__| \n");
+
+        System.out.println(s);
+        /*
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+
+         */
+    }
+}
