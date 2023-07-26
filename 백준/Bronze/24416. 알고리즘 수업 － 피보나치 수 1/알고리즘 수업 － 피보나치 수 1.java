c0b83abae06c68994bc6536729c942226b2c51c6
NoJaeHyuk <32382839+NoJaeHyuk@users.noreply.github.com>
2023-07-26 23:16:52 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 584 ms, Memory: 18500 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    static long dp[];
+    static int c1 = 0;
+    static int c2 = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        fib(n);
+
+        dp = new long[n+1];
+
+        dp[1] = 1;
+        dp[2] = 1;
+
+        fibonacci(n);
+
+        System.out.println(c1+" "+c2);
+
+    }
+
+    public static long fib(int n){
+        if(n == 1 || n == 2){
+            c1++;
+            return 1;
+        }
+
+        return fib(n-1) + fib(n-2);
+    }
+
+    public static long fibonacci(int n){
+
+        for(int i=3; i<=n; i++){
+            c2++;
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+
+        return dp[n];
+    }
+}
