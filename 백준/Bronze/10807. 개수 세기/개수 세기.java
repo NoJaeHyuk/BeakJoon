28802ba6580d034ba01f901b860a37898db51796
NoJaeHyuk <32382839+NoJaeHyuk@users.noreply.github.com>
2023-08-04 11:15:28 +0900

    [Bronze V] Title: 개수 세기, Time: 232 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int count = 0;						 // b 와 일치할시 갯수를 올리는 변수
+        int N = sc.nextInt();
+        int[] arr = new int[N];				 // 배열 생성, 길이는 N만큼
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();			 // 배열에 입력한 정수들 넣어주기
+        }
+        int b = sc.nextInt();
+
+        for(int j=0; j< arr.length; j++){
+            if(b == arr[j]){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
