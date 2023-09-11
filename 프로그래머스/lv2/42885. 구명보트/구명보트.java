ef61e8a8ae4d54422a4d36fd847df3af12ab487e
JH_DEV <32382839+NoJaeHyuk@users.noreply.github.com>
2023-09-11 16:51:01 +0900

    [level 2] Title: 구명보트, Time: 10.28 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+
+        Arrays.sort(people);
+
+        int start = 0;
+
+        for(int end = people.length-1; end >= start; end--){
+            if(people[start] + people[end] <= limit){
+                start++;
+            }
+            answer++;
+        }
+
+        System.out.println(answer);
+
+        return answer;
+    }
+}
