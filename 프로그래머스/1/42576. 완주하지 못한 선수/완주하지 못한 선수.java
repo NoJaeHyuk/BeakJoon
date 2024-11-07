07a764df260e722e964ce6c7cd4eb27b849e3616
JH_DEV <32382839+NoJaeHyuk@users.noreply.github.com>
2024-11-07 18:35:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 97.41 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,25 +2,25 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        
         Map<String, Integer> countSet = new HashMap<>();
         
+        // 참가자 명단을 해시맵에 저장하면서 카운트
         for (String str : participant) {
             countSet.put(str, countSet.getOrDefault(str, 0) + 1);
         }
         
+        // 완주자 명단을 순회하며 카운트를 감소
         for (String com : completion) {
-            countSet.put(com, countSet.getOrDefault(com, 0) - 1);
-            if(countSet.get(com) == 0){
-                countSet.remove(com);
+            countSet.put(com, countSet.get(com) - 1);
+        }
+        
+        // 카운트가 0이 아닌 키가 완주하지 못한 선수
+        for (Map.Entry<String, Integer> entry : countSet.entrySet()) {
+            if (entry.getValue() > 0) {
+                return entry.getKey();
             }
         }
         
-        for (String key : countSet.keySet()) {
-            answer = key;
-        }
-        
-        return answer;
+        return ""; // 모든 참가자가 완주한 경우 빈 문자열 반환 (문제 조건에선 발생하지 않음)
     }
 }
