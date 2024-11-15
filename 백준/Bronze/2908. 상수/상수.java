54c578c682709d2c6b7df36ee7ee8712a15e6f0b
JH_DEV <32382839+NoJaeHyuk@users.noreply.github.com>
2024-11-15 12:45:11 +0900

    [Bronze II] Title: 상수, Time: 112 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] numbers = br.readLine().split(" ");
+
+        int reversedA = reverseNumber(numbers[0]);
+        int reversedB = reverseNumber(numbers[1]);
+
+        System.out.println(Math.max(reversedA, reversedB));
+    }
+
+    private static int reverseNumber(String number) {
+        StringBuilder reversed = new StringBuilder(number).reverse();
+        return Integer.parseInt(reversed.toString());
+    }
+}
