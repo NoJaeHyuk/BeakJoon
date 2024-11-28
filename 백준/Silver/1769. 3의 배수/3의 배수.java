d1654e3ff7743c2c7dd62a05cda1075ced88f584
JH_DEV <32382839+NoJaeHyuk@users.noreply.github.com>
2024-11-28 15:13:39 +0900

    [Silver V] Title: 3의 배수, Time: 232 ms, Memory: 22052 KB -BaekjoonHub

--- 백준/Silver/1769. 3의 배수/3의 배수.java
+++ 백준/Silver/1769. 3의 배수/3의 배수.java
@@ -6,19 +6,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String x = br.readLine(); // 입력값을 String으로 읽음
+        String x = br.readLine();
         int transform = 0;
 
-        while (x.length() > 1) { // 한 자리 숫자가 될 때까지 반복
+        while (x.length() > 1) {
             int sum = 0;
             transform++;
-            for (char ch : x.toCharArray()) { // 각 자릿수 합산
+            for (char ch : x.toCharArray()) {
                 sum += ch - '0';
             }
-            x = String.valueOf(sum); // 합계를 문자열로 변환
+            x = String.valueOf(sum);
         }
 
-        // 최종 출력
         System.out.println(transform);
         if (Integer.parseInt(x) % 3 == 0) {
             System.out.println("YES");
