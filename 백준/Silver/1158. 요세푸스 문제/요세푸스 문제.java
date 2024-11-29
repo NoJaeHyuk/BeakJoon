290d1b61725e8e861bafa84aced8627f4ebf8108
JH_DEV <32382839+NoJaeHyuk@users.noreply.github.com>
2024-11-29 11:18:28 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 152 ms, Memory: 15060 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,35 +1,47 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Queue;
+import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.List;
 import java.util.LinkedList;
 
+/**
+ * 연결리스트 활용
+ */
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        Queue queue = new LinkedList<>();
-
+        List<Integer> list = new LinkedList<>();
         for (int i = 0; i < n; i++) {
-            queue.add(i + 1);
+            list.add(i + 1);
         }
 
-        StringBuilder result = new StringBuilder();
-        result.append("<");
-        while (!queue.isEmpty()) {
-            for (int i = 0; i < k -1; i++) {
-                queue.add(queue.poll());
-            }
-            result.append(queue.poll()); // K번째 숫자 제거
-            if (!queue.isEmpty()) {
-                result.append(", ");
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        int index = 0;
+        while (!list.isEmpty()) {
+            // 원형 큐, 회전초밥 ...
+            // 인덱스의 k번째를 더하되 인덱스이기 때문에 -1 처리
+            // n값을 넘으면 안되니깐 n으로 나눈 나머지 값을 이용
+            index = (index + k - 1) % list.size();
+            sb.append(list.get(index));
+            list.remove(index);
+
+            if (!list.isEmpty()) {
+                sb.append(", ");
             }
         }
-        result.append(">");
-        System.out.println(result);
+        
+        sb.append(">");
+        System.out.println(sb);
     }
+
 }
+
+
