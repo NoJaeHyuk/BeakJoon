0e2733719f6bffd1921e90850f7901a8067e3fa7
JH_DEV <32382839+NoJaeHyuk@users.noreply.github.com>
2024-12-03 09:18:36 +0900

    [Silver V] Title: 식당 입구 대기 줄, Time: 328 ms, Memory: 40896 KB -BaekjoonHub

--- 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.java
+++ 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.java
@@ -3,33 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
         Queue<Integer> queue = new LinkedList<>();
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
+        int findStdunt = Integer.MAX_VALUE;
+        int maxCount = 0;
         for (int i = 0; i < n; i++) {
-            String[] inputs = br.readLine().split(" ");
-            if (Integer.parseInt(inputs[0]) == 1) {
-                int value = Integer.parseInt(inputs[1]);
-                queue.add(value);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
 
-                // 큐의 크기 업데이트 시 최대 크기와 최소 값 관리
-                if(queue.size() > max) {
-                    max = queue.size();
-                    min = value; // 새로운 최대 크기 도달 시 최소 값 초기화
-                } else if(queue.size() == max) {
-                    min = Math.min(min, value); // 최대 크기와 동일한 경우 최소 값 업데이트
+            if (command == 1) {
+                int studentNumber = Integer.parseInt(st.nextToken());
+                queue.add(studentNumber);
+                if (maxCount <= queue.size()) {
+                    if (maxCount == queue.size()) {
+                        findStdunt = Math.min(findStdunt, studentNumber);
+                    } else {
+                        findStdunt = studentNumber;
+                    }
+                    maxCount = queue.size();
                 }
             } else {
                 queue.poll();
             }
         }
 
-        System.out.println(max + " " + min);
+        System.out.println(maxCount + " " + findStdunt);
     }
 }
