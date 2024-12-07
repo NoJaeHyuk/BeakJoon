32db128d737d1f174ab7a1e084aedc9e98532d83
JH_DEV <32382839+NoJaeHyuk@users.noreply.github.com>
2024-12-07 17:40:27 +0900

    [Silver II] Title: 촌수계산, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,77 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
-/**
- * 문제 분석
- * 주어진 두 사람에 대한 촌수를 구하는 문제이다.
- * 입력값은 전체 사람의 수 n
- * 두번째줄에는 두 사람의 관계 start, end
- * 세번째줄에는 부모자식의 관계 수 m
- * <p>
- * 문제 풀이
- * DFS로 접근
- * 입력값은 인접행렬(배열) 받는다.
- * <p>
- * DFS(start, end, count)
- * 사람에 대한 방문을 체크하는 방문배열 - 일차원배열
- * 1. 방문배열 상태를 체크
- * 2. for문 n번
- * 3. 방문하지 않는 사람 체크, 연결이 된 여부
- * 4. dfs(i, end, count + 1);
- * 5. 재귀가 끝나는 조건  start == end 같아지면 return
- */
 public class Main {
-    static int n;
-    static int[][] arr;
+    static List<List<Integer>> list = new ArrayList<>();
     static boolean[] visited;
-    static int result = -1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
+
+        int targetA = Integer.parseInt(st.nextToken());
+        int targetB = Integer.parseInt(st.nextToken());
+
         int m = Integer.parseInt(br.readLine());
 
-        arr = new int[n + 1][n + 1];
         visited = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
 
         for (int i = 0; i < m; i++) {
             StringTokenizer st1 = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st1.nextToken());
             int y = Integer.parseInt(st1.nextToken());
 
-            // 무방향, 양방향 그래프
-            arr[x][y] = 1;
-            arr[y][x] = 1;
+            list.get(x).add(y);
+            list.get(y).add(x);
         }
 
-        dfs(start, end, 0);
-        System.out.println(result);
+        System.out.println(bfs(targetA, targetB));
     }
 
-    /**
-     * 1. 방문배열 상태를 체크
-     * 2. for문 n번
-     * 3. 방문하지 않는 사람 체크, 연결이 된 여부
-     * 4. dfs(i, end, count + 1);
-     * 5. 재귀가 끝나는 조건  start == end 같아지면 return
-     */
-    private static void dfs(int start, int end, int count) {
+    private static int bfs(int start, int end) {
         visited[start] = true;
 
-        if (start == end) {
-            result = count;
-            return;
-        }
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{start, 0});
 
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i] && arr[start][i] == 1) {
-                dfs(i, end, count + 1);
+        while (!queue.isEmpty()) {
+            int[] inputs = queue.poll();
+            int node = inputs[0];
+            int count = inputs[1];
+
+            if (node == end) {
+                return count;
+            }
+
+            for (int neighbor : list.get(node)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(new int[]{neighbor, count + 1});
+                }
             }
         }
+
+        return -1;
     }
 }
