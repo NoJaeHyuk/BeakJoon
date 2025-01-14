162b6aaf82456f174bc4e13669cc3565481b8868
JH_DEV <32382839+NoJaeHyuk@users.noreply.github.com>
2025-01-14 09:47:58 +0900

    [Silver IV] Title: 암기왕, Time: 1420 ms, Memory: 285312 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -10,13 +10,12 @@ public class Main {
         for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine()); // 첫 번째 수첩 크기
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int[] note1 = new int[N];
+            Set<Integer> note1 = new HashSet<>();
 
             // 첫 번째 수첩 숫자 저장
             for (int i = 0; i < N; i++) {
-                note1[i] = Integer.parseInt(st.nextToken());
+                note1.add(Integer.parseInt(st.nextToken()));
             }
-            Arrays.sort(note1); // 정렬
 
             int M = Integer.parseInt(br.readLine()); // 두 번째 수첩 크기
             st = new StringTokenizer(br.readLine());
@@ -24,7 +23,7 @@ public class Main {
             // 두 번째 수첩 숫자 확인
             for (int i = 0; i < M; i++) {
                 int num = Integer.parseInt(st.nextToken());
-                if (binarySearch(note1, num)) {
+                if (note1.contains(num)) {
                     sb.append("1\n");
                 } else {
                     sb.append("0\n");
@@ -32,21 +31,5 @@ public class Main {
             }
         }
         System.out.print(sb);
-    }
-
-    // 이분 탐색 메서드
-    private static boolean binarySearch(int[] arr, int target) {
-        int left = 0, right = arr.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == target) {
-                return true;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return false;
     }
 }
