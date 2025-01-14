a6909dd79a6102cd95d8121060537a79251d3e76
JH_DEV <32382839+NoJaeHyuk@users.noreply.github.com>
2025-01-14 09:47:41 +0900

    [Silver IV] Title: 암기왕, Time: 1700 ms, Memory: 225232 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,39 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine()); // 첫 번째 수첩 크기
             StringTokenizer st = new StringTokenizer(br.readLine());
-            Map<Integer, Integer> countMap = new HashMap<>();
+            int[] note1 = new int[N];
 
-            for (int j = 0; j < N; j++) {
-                int x = Integer.parseInt(st.nextToken());
-                countMap.put(x, countMap.getOrDefault(x, 0) + 1);
+            // 첫 번째 수첩 숫자 저장
+            for (int i = 0; i < N; i++) {
+                note1[i] = Integer.parseInt(st.nextToken());
             }
+            Arrays.sort(note1); // 정렬
 
-            int M = Integer.parseInt(br.readLine());
-            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(br.readLine()); // 두 번째 수첩 크기
+            st = new StringTokenizer(br.readLine());
 
-            for (int j = 0; j < M; j++) {
-                int x = Integer.parseInt(st1.nextToken());
-                if (countMap.containsKey(x)) {
-                    sb.append(1).append("\n");
+            // 두 번째 수첩 숫자 확인
+            for (int i = 0; i < M; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (binarySearch(note1, num)) {
+                    sb.append("1\n");
                 } else {
-                    sb.append(0).append("\n");
+                    sb.append("0\n");
                 }
             }
         }
-        System.out.println(sb);
+        System.out.print(sb);
+    }
+
+    // 이분 탐색 메서드
+    private static boolean binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                return true;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
     }
 }
