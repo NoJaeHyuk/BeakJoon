6d6ca1c4f8f2ed809b43299998019914bec19faf
JH_DEV <32382839+NoJaeHyuk@users.noreply.github.com>
2025-01-23 11:35:53 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -12,7 +12,6 @@ public class Main {
     static char[][] map;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static int sum = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,37 +23,36 @@ public class Main {
             map[i] = br.readLine().toCharArray();
         }
 
-        List<Integer> arr = new ArrayList<>();
-        int count = 0;
+        List<Integer> complexSizes = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (map[i][j] == '1' && !visited[i][j]) {
-                    dfs(i, j);
-                    count++;
-                    arr.add(sum);
-                    sum = 0;
+                    int size = dfs(i, j);
+                    complexSizes.add(size);
                 }
             }
         }
 
-        System.out.println(count);
-        Collections.sort(arr);
-        for (Integer a : arr) {
-            System.out.println(a);
+        System.out.println(complexSizes.size());
+        Collections.sort(complexSizes);
+        for (Integer size : complexSizes) {
+            System.out.println(size);
         }
     }
 
-    private static void dfs(int x, int y) {
+    private static int dfs(int x, int y) {
         visited[x][y] = true;
-        sum++;
+        int size = 1;
 
         for (int i = 0; i < dx.length; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
             if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny] && map[nx][ny] == '1') {
-                dfs(nx, ny);
+                size += dfs(nx, ny);
             }
         }
+
+        return size;
     }
 }
