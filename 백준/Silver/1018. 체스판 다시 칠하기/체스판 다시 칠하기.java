b81a7589b3ed4edd3dbd0397f68f3875ecd46479
JH_DEV <32382839+NoJaeHyuk@users.noreply.github.com>
2025-02-04 14:03:07 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -4,66 +4,60 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[][] a;
-    static int MIN = 64;
+    public static final int SIZE = 8;
+    static boolean[][] board;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        a = new boolean[N][M];
+        board = new boolean[N][M];
 
-        for(int i = 0; i < N; i++){
-            String str = br.readLine();
-            for(int j = 0; j < M; j++){
-                if(str.charAt(j) == 'W'){
-                    a[i][j] = true;
-                }else{
-                    a[i][j] = false;
+        for (int i = 0; i < N; i++) {
+            String ary = br.readLine();
+            for (int j = 0; j < M; j++) {
+                if (ary.charAt(j) == 'B') {
+                    board[i][j] = true;
                 }
             }
         }
 
-        //8x8 만들수 있는 경우의 수
-        int r1 = N - 7;
-        int r2 = M - 7;
+        int min = Integer.MAX_VALUE;
 
-        for(int i = 0; i < r1; i++){
-            for(int j = 0; j < r2; j++){
-                find(i,j);
+        // 모든 8x8을 탐색할 수 있는 조건
+        for (int i = 0; i + SIZE <= N; i++) {
+            for (int j = 0; j + SIZE <= M; j++) {
+                // 체크판을 칠하는 조건 카운터
+                min = Math.min(min, countChangedColor(i , j));
             }
         }
 
-        System.out.println(MIN);
+        System.out.println(min);
     }
 
-    public static void find(int i, int j){
-        int end_x = i + 8;
-        int end_y = j + 8;
+    private static int countChangedColor(int r_start, int c_start) {
+        int r_end = r_start + SIZE;
+        int c_end = c_start + SIZE;
+
+        boolean firstColor = board[r_start][c_start];
         int count = 0;
 
-        //첫 위치가 기준
-        boolean chk = a[i][j];
+        for (int i = r_start; i < r_end; i++) {
+            boolean color = firstColor;
 
-        for(int i1 = i; i1<end_x; i1++) {
-            for (int j1 = j; j1 < end_y; j1++) {
-                if (a[i1][j1] != chk) {
+            for (int j = c_start; j < c_end; j++) {
+                if (board[i][j] != color) {
                     count++;
                 }
-
-                //다음 순서의 값 지정 한다고 생각하자
-                chk = !chk;
+                color = !color;
             }
-            //새로운 행의 시작은 반대이므로 해당 처리
-            chk = !chk;
+
+            firstColor = !firstColor;
         }
 
-        count = Math.min(count,64-count);
-
-        MIN = Math.min(MIN,count);
+        return Math.min(count, (SIZE * SIZE) - count);
     }
 }
